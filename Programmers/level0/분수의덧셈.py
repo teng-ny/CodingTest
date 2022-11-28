fe116b09b56ea49a737d576381ae98a764617d7a
박태은 <96412996+teng-ny@users.noreply.github.com>
2022-11-28 22:41:55 +0900

    Add files via upload

---
+++ Programmers/level0/분수의덧셈.py
@@ -0,0 +1,10 @@
+import fractions
+
+def solution(denum1, num1, denum2, num2):
+    a=fractions.Fraction(denum1, num1)
+    b=fractions.Fraction(denum2, num2)
+    합=a+b
+    분자=합.numerator
+    분모=합.denominator
+    answer = [분자, 분모]
+    return answer
